198f79cefc00629687d89c5dc3c13805f1f281bd
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-07-01 18:30:27 +0100

    the search-bar is completed

---
+++ .history/try/src/app/components/search-bar/search-bar.component_20230701182254.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent implements OnInit{
+
+    constructor(private router: ) {}
+
+    ngOnInit(): void {}
+}
